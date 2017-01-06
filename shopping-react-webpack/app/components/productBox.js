fe70ddad5cd19f843b5d4c5e90fb6779cca8822c
xiaojinfeng <fengshao74110@126.com>
2017-01-06 18:44:42 +0800

    test

---
+++ shopping-react-webpack/app/components/productBox.js
@@ -0,0 +1,16 @@
+/**
+ * Created by fengshao on 2016/9/14.
+ */
+var React = require('react');
+var ProductBox = React.createClass({
+	render: function () {
+		return (
+			<div className="productBox">
+				Hello World!
+			</div>
+		);
+	}
+});
+
+module.exports = ProductBox;
+
