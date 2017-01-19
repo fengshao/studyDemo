63a8548e30c06f9433d35c4cb7f26308ac14871a
xiaojinfeng <fengshao74110@126.com>
2017-01-19 15:42:46 +0800

    click 两次修复

--- app/app/script/iscroll.js
+++ app/app/script/iscroll.js
@@ -563,24 +563,24 @@
 							}, 200); // 200 is default zoom duration
 						}
 					} else if (this.options.handleClick) {
-						that.doubleTapTimer = setTimeout(function () {
-							that.doubleTapTimer = null;
+						//that.doubleTapTimer = setTimeout(function () {
+						//	that.doubleTapTimer = null;
 
-							// Find the last touched element
-							target = point.target;
-							while (target.nodeType != 1) target = target.parentNode;
+						// Find the last touched element
+						//target = point.target;
+						//while (target.nodeType != 1) target = target.parentNode;
 
-							//if (target.tagName != 'SELECT' && target.tagName != 'INPUT' && target.tagName != 'TEXTAREA') {
-							//	ev = doc.createEvent('MouseEvents');
-							//	ev.initMouseEvent('click', true, true, e.view, 1,
-							//		point.screenX, point.screenY, point.clientX, point.clientY,
-							//		e.ctrlKey, e.altKey, e.shiftKey, e.metaKey,
-							//		0, null);
-							//	ev._fake = true;
-							//	target.dispatchEvent(ev);
-							//
-							//}
-						}, that.options.zoom ? 250 : 0);
+						//if (target.tagName != 'SELECT' && target.tagName != 'INPUT' && target.tagName != 'TEXTAREA') {
+						//	ev = doc.createEvent('MouseEvents');
+						//	ev.initMouseEvent('click', true, true, e.view, 1,
+						//		point.screenX, point.screenY, point.clientX, point.clientY,
+						//		e.ctrlKey, e.altKey, e.shiftKey, e.metaKey,
+						//		0, null);
+						//	ev._fake = true;
+						//	target.dispatchEvent(ev);
+						//
+						//}
+						//}, that.options.zoom ? 250 : 0);
 					}
 				}
 
