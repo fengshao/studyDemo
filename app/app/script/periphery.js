c3a6eab9ac9375d1de90bd03584c0a898dea07f1
xiaojinfeng <fengshao74110@126.com>
2017-03-15 10:41:59 +0800

    APP 首页调整

--- app/app/script/periphery.js
+++ app/app/script/periphery.js
@@ -1,64 +1,18 @@
 /**
  * Created by fengs on 2016/9/19.
  */
-var maxPage = 1, curPage = 1, wrapper = null;
 $(function () {
 
 	var parms = {
 		cliWH: "",
 		cliHG: "",
 		noImgUrl: "./app/image/",
-		curPage: 1,
-		maxPage: 1,
-		pageSize: 6,
 		scrollState: false
 	};
 
 	renderDom();
+	//$("img").lazyload();
 
-	function Refresh() {
-		setTimeout(function () {
-			window.location.reload(true);
-		}, 1000);
-
-	};
-
-	function Load() {
-		if (!parms.scrollState) {
-			parms.scrollState = true;
-			setTimeout(function () {
-				reftest();
-				wrapper.refresh();
-				parms.scrollState = false;
-			}, 1000);
-		}
-	};
-
-	function scorllInit() {
-		refresher.init({
-			id: "wrapper",
-			pullDownAction: Refresh,
-			pullUpAction: Load,
-			ifALL: parms.curPage == parms.maxPage
-		});
-	};
-
-	function reftest() {
-		var special_data = data.special_data,
-			timestamp = data.timestamp;
-		parms.curPage++;
-		curPage = parms.curPage;
-		var datas = {
-			list: special_data.slice((parms.curPage - 1) * 6, parms.curPage * parms.pageSize - 1),
-			className: "special-content-div",
-			clickType: "special_",
-			noImgUrl: parms.noImgUrl + "special.jpg?v=" + timestamp
-		};
-
-		var html = template('publicTemplate', datas);
-		$('#special-content').append(html);
-		$(".special-content-div").css("height", (parms.cliWH - 20) / 710 * 325)
-	};
 
 	function layoutInit() {
 		parms.cliWH = document.documentElement.clientWidth;
@@ -68,11 +22,7 @@ $(function () {
 			advContainer.style.width = document.documentElement.clientWidth + "px";
 			advContainer.style.height = (parms.cliWH / 375 * 133) + "px";
 		}
-		jQuery.Deferred().done(function () {
-			getImgAjax();
-		}, function () {
-			scorllInit();
-		}).resolve();
+		getImgAjax();
 	};
 
 	function renderDom() {
@@ -85,10 +35,6 @@ $(function () {
 			$(".periphery-content").show();
 			$(".no-data-content").hide();
 
-			if (data.special_data && data.special_data.length > 0) {
-				maxPage = parms.maxPage = Math.ceil(data.special_data.length / parms.pageSize);
-				curPage = parms.curPage;
-			}
 			var datas = {
 				list: data.specilTypeList
 			};
@@ -108,64 +54,80 @@ $(function () {
 
 	function getImgAjax() {
 		var worth_buying_data = data.worth_buying_data,
-			main_activity_data = data.main_activity_data,
 			first_product_data = data.first_product_data,
 			special_data = data.special_data,
 			timestamp = data.timestamp;
 
-		if (main_activity_data && main_activity_data.length > 0) {
-			adObj(main_activity_data.slice(0, 6));
-		} else {
-			$(".main-activity").hide();
-		}
-
 		if (worth_buying_data && worth_buying_data.length > 0) {
+			parms.cliWH = document.documentElement.clientWidth;
+			parms.cliHG = document.documentElement.clientHeight;
+			var leftWidth = parms.cliWH * 0.4;
+			var leftHeight = leftWidth / 300 * 340;
+			var rightWidth = parms.cliWH - 36 - leftWidth;
+			var rightHeight = 80 * rightWidth / 189;
+
 			var datas = {
 				leftData: worth_buying_data[0],
 				rightDataTop: worth_buying_data[1],
 				rightDataBottom: worth_buying_data[2],
-				noLeftImgUrl: parms.noImgUrl + "worth-buying-left.jpg?v=" + timestamp,
-				noRightImgUrl: parms.noImgUrl + "worth-buying-right.jpg?v=" + timestamp
+				noLeftImgUrl: parms.noImgUrl + "worth-buying-left.jpg?isNeedDownload=true&level=1&v=" + timestamp,
+				noRightImgUrl: parms.noImgUrl + "worth-buying-right.jpg??isNeedDownload=true&level=1&v=" + timestamp,
+				leftWidth: leftWidth,
+				leftHeight: leftHeight,
+				rightWidth: rightWidth,
+				rightHeight: rightHeight
 			};
 			var html = template('worthBuyingTemplate', datas);
 			document.getElementById('worth-buying-content').innerHTML = html;
-			parms.cliWH = document.documentElement.clientWidth;
-			parms.cliHG = document.documentElement.clientHeight;
-			var leftWidth = (parms.cliWH - 28) * 0.432;
 
-			$(".worth-buying-left-div ").css("width", leftWidth).css("height", leftWidth / 300 * 340);
-			$(".worth-buying-right-div ").css("width", parms.cliWH - 28 - leftWidth);
+			$(".worth-buying-left-div ").css("width", leftWidth).css("height", leftHeight);
+			$(".worth-buying-right-div ").css("width", rightWidth);
+			$(".worth-buying-right-img-div1,.worth-buying-right-img-div2").css("width", rightWidth).css("height", rightHeight);
 		} else {
 			$(".worth-buying").hide();
 			$(".worth-buying").next().hide();
 		}
 
 		if (first_product_data && first_product_data.length > 0) {
+			var divWidth = (parms.cliWH - 46) / 3;
+			var divHeight = divWidth / 220 * 426;
+
 			var datas = {
 				list: first_product_data.slice(0, 6),
 				className: "first-product-content-div",
 				clickType: "recommend_",
-				noImgUrl: parms.noImgUrl + "first-product.jpg?v=" + timestamp
+				noImgUrl: parms.noImgUrl + "first-product.jpg?isNeedDownload=true&level=3&v=" + timestamp,
+				divHeight: divHeight,
+				divWidth: divWidth,
+				levelType: '3'
+
 			};
 			var html = template('publicTemplate', datas);
 			document.getElementById('first-product-content').innerHTML = html;
-			var divWidth = (parms.cliWH - 50) / 3;
-			$(".first-product-content-div").css("width", divWidth).css("height", divWidth / 226 * 312);
+
+			$(".first-product-content-div").css("width", divWidth).css("height", divHeight);
 		} else {
 			$(".first-product").hide();
 			$(".first-product").next().hide();
 		}
 
 		if (special_data && special_data.length > 0) {
+			var divHeight = (parms.cliWH - 26) / 349 * 326;
+			var divWidth = parms.cliWH - 20;
+
 			var datas = {
-				list: special_data.slice(0, 6),
+				list: special_data,
 				className: "special-content-div",
 				clickType: "depth_focusing_",
-				noImgUrl: parms.noImgUrl + "special.jpg?v=" + timestamp
+				noImgUrl: parms.noImgUrl + "special.jpg?isNeedDownload=true&level=3&v=" + timestamp,
+				divHeight: divHeight,
+				divWidth: divWidth,
+				levelType: '4'
 			};
 			var html = template('publicTemplate', datas);
 			document.getElementById('special-content').innerHTML = html;
-			$(".special-content-div").css("height", (parms.cliWH - 20) / 710 * 326)
+			$(".special-content-div").css("width", parms.cliWH - 26)
+				.css("height", (parms.cliWH - 26) / 349 * 81);
 		} else {
 			$(".special").hide();
 		}
