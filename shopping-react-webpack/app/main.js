1413afb5ef398d7624c198d81b1e18bcfb58faa6
xiaojinfeng <xiaojinfeng@wesai.com>
2017-07-05 15:14:10 +0800

    删除多余文件 修改文件名

--- shopping-react-webpack/app/main.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by fengshao on 2016/9/14.
- */
-var React = require('react');
-var ReactDOM = require('react-dom');
-var AppComponent = require('./components/productBox.js');
-ReactDOM.render(<AppComponent />, document.getElementById('content'));
